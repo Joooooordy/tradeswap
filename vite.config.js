0848ed7de38e09b2c86b0b4d7d3ab2f3aff03eb2
Jordy Breur <jjbreur@outlook.com>
2025-02-07 17:46:23 +0100

    configs

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
     server: {
-        host: '0.0.0.0',   // Make it accessible on all interfaces (external access)
-        port: 5173,         // Port for Vite server
+        host: '0.0.0.0',  // Make it accessible on all interfaces (external access)
+        port: 8080,
         hmr: {
-            host: 'localhost', // WebSocket connection for hot reloading
-            port: 5173,        // WebSocket server port
+            host: 'localhost',  // WebSocket connection for hot reloading
+            port: 8080,        // WebSocket server port
+            protocol: 'wss',   // Use WebSocket Secure (WSS) instead of WebSocket (WS)
         },
+        watch: {
+            usePolling: true
+        }
     },
     plugins: [
         laravel({
-            input: ['public/resources/css/app.scss', 'resources/js/app.js'],
+            input: ['/public/resources/css/app.scss', '/public/resources/js/app.js'],
             refresh: true,
         }),
+        basicSsl(),
     ],
 });
